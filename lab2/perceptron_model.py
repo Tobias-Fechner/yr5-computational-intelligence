76fbde5e7f0d81c1c9a3ff9b105435678f2b731f
Tbs_Fchnr <tobias.fechner@hotmail.co.uk>
2020-10-16 14:20:17 +0200

    remove TODO and commented out sections

--- lab2/perceptron_model.py
+++ lab2/perceptron_model.py
@@ -126,25 +126,3 @@ def meanSquareError(targets, outputs):
     else:
         error = 1/2 * np.sum(abs(np.array(targets) - np.array(outputs)) ** 2)
         return error
-
-# TODO: Delete below when no longer in use
-
-# #### Exercises
-# ## Ex1.1
-# file = open("ex1.txt", "w")
-#
-# for item in inputs:
-#     file.write("Ex1.1\nInputs: {0}\nWeights: {1}\nBias: {2}\nResult: {3}\n\n".format(item, weights, bias, perceptron(item, weights, bias)))
-# file.close()
-#
-# ## Ex1.2
-# # Interpreting this information as a truth table, what logical function is being performed?
-# file = open("ex1.txt", "a")
-# file.write("Ex1.2\nQ: Interpret as truth table, what logical function is it?\nA: AND\n\n")
-# file.close()
-#
-# ## Ex1.3
-# # What are the weight vectors and the bias for the logical functions: AND,OR,NAND, NOR and XOR?
-# file = open("ex1.txt", "a")
-# file.write("Ex1.3\nOR: bias = 0, weights [[0.5,0.5], [1,1]]\nNAND: requires NOT operator\nNOR: requires NOT operator\nXOR: requires NOT operator.\n "
-#            "These truth tables are not linearly separable.\n\n")
