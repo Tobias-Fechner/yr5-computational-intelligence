d916b4f019528e8ed2ee053b68a0e395a6631f33
Tbs_Fchnr <tobias.fechner@hotmail.co.uk>
2020-11-30 10:45:35 +0100

    Initial commit for ga assignment

---
+++ ga_polynomial.py
@@ -0,0 +1,149 @@
+from random import randint, random, choice
+from math import ceil
+import logging
+import numpy as np
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+
+def individual(min, max, length):
+    """
+    Function creates a single individual of the population using a random integer between the min/ max parameter inputs.
+    :param length: number of random ints to generate
+    :param min: min int of range
+    :param max: max int of range
+    :return: returns list of size 'length' random numbers within min/max range
+    """
+    return np.random.randint(min, max, length)
+
+def population(count, min, max, length):
+    """
+    Creates a population of individuals.
+    :param count: the number of individuals in the population
+    :param length: the number of values per individual
+    :param min: the minimum possible value in an individual's list of values
+    :param max: the maximum possible value in an individual's list of values
+    :return: returns a list of individuals of size 'length
+    """
+    return [ individual(min, max, length) for x in range(count) ]
+
+def fitness(individual):
+    """
+    Determines the fitness of a given individual by calculating MSE between prediction and target over a range of values of x.
+    :param individual: individual to be measured
+    :return: fitness metric
+    """
+
+    # Extract polynomial coefficients from individual
+    a1 = individual[0]
+    a2 = individual[1]
+    a3 = individual[2]
+    a4 = individual[3]
+    a5 = individual[4]
+    c = individual[5]
+
+    y = []
+    errors = []
+
+    # For a range of values of x, calculate the target and predicted value of y
+    for x in np.linspace(-1,1, 20):
+        yTarget = 25*x**5 + 18*x**4 + 0 - 14*x**2 + 7*x -19
+        yPredict = a1*x**5 + a2*x**4 + a3*x**3 + a4*x**2 + a5*x + c
+
+        # Store target and prediction as pair in list of tuples
+        y.append((yTarget, yPredict))
+
+    # Iterate over list of tuples to calculate square of error between target and prediction
+    for target, prediction in y:
+        error = abs(target - prediction)
+        #squared = error**2
+
+        # Store square of error in list
+        errors.append(error)
+
+    # Return mean of squared errors as fitness value for individual
+    return sum(errors)/len(errors)
+
+def grade(population):
+    """
+    Grade the overall population by finding the average fitness.
+    :param population:
+    :return:
+    """
+    return np.average([fitness(x) for x in population])
+
+def evolve(population, retain=0.2, random_select=0.05, mutate=0.01, femalePortion=0.5):
+
+    # Assert all fractions between 0-1
+    assert 0 <= femalePortion <= 1
+    assert 0 <= random_select <= 1
+    assert 0 <= mutate <= 1
+    assert 0 <= retain <= 1
+
+    # Grade each individual within the population
+    individualGrades = [(fitness(x), list(x)) for x in population]
+
+    # Rank population based on individual grades
+    graded = [x[1] for x in sorted(individualGrades)]
+
+    # Calculate number of individuals to retain as parents of the next generation
+    retain_length = ceil(len(graded)*retain)
+
+    # Retain the desired number of elements from the leading side of the list as the parents of next generation
+    parents = graded[:retain_length]
+
+    # Add other individuals to parents group to promote genetic diversity
+    for individual in graded[retain_length:]:
+
+        # Configured probability used to decide if each individual from remaining population is added to parent gene pool
+        if random_select > random():
+            parents.append(individual)
+
+    # Mutate some individuals
+    for individual in parents:
+
+        # Configured probability used to decide if each individual from parent gene pool is mutated
+        if mutate > random():
+
+            # Generate value to randomly index individual's contents
+            randIndex = choice(range(len(individual)))
+
+            # Generate a random integer between the individual's min/ max and store value using random index
+            individual[randIndex] = randint(min(individual), max(individual))
+
+    children = []
+    lenParents = len(parents)
+    desiredChildren = len(population) - lenParents
+
+    # Create children from high performing parent group to maintain population size
+    while len(children) < desiredChildren:
+
+        # Get female and male index
+        femaleIndex = randint(0, lenParents-1)
+        maleIndex = randint(0, lenParents-1)
+
+        if maleIndex == femaleIndex:
+            pass
+
+        else:
+
+            # Get female and male values
+            female = parents[femaleIndex]
+            male = parents[maleIndex]
+
+            # Determine proportion of female chromosomes to
+            split = int(len(female)*femalePortion)
+            logging.info("Split at {}. Parent chromosomes: {}; Child gets {} from female and {} from male.".format(
+                split, len(male), len(female[:split]), len(male[split:])))
+
+            # Split individual chromosomes based on desired female portion of chromosomes
+            # Create child with half of male chromosomes and other half of female's chromosomes
+            child = np.append(male[:split], female[split:])
+            logging.info("Child made with {} chromosomes.".format(len(child)))
+
+            # Add child to list
+            children.append(child)
+
+    # Return list of children and parent individuals to be next generation
+    parents.extend(children)
+    return parents, individualGrades
